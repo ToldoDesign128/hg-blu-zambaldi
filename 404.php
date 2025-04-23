f23b5ead35ef3adc64d350f14ef2727a789ea09d
Federico <fede.toldo96@gmail.com>
2025-04-23 14:45:28 +0200

    Page 404

--- 404.php
+++ 404.php
@@ -1,6 +1,15 @@
 <?php
 get_header(); ?>
 <main class="error">
-    
+    <section class="hero-error">
+        <h1 class="title title-h1 medium black-title uppercase">
+            404
+            <span class="title-bg semibold uppercase">
+                404
+            </span>
+        </h1>
+        <p class="text">Non siamo riusciti a trovare la pagina che cerchi</p>
+        <a class="btn-red" href="<?php echo home_url(); ?>">Torna alla Home</a>
+    </section>
 </main>
 <?php get_footer(); ?>
